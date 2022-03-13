27a2cd9c9fdf4820b554c76c4ab8dd42cd3ff2cc
AdamBoley <adamboley92@gmail.com>
2022-03-13 15:26:41 +0000

    sort into folders

--- js/assignment-operators.js
+++ js/assignment-operators.js
@@ -1,11 +1,11 @@
-let num = 10
-console.log(num)
+let num = 10 //declares a variable called num, assigns value of 10
+console.log(num) //logs 10
 
-num += 10 //addition assignment
-console.log(num)
+num += 10 //adds 10 to the value of num
+console.log(num) //logs 20
 
-let numB = 10
+let numB = 10 //declares variable numB, assigns 10
 console.log(numB)
 
-numB *= 10 //multiplication assignment
-console.log(numB)
+numB *= 10 //multiplies by 10
+console.log(numB) //logs 100
---
+++ js/get-elements/get-by-tag-name/tag-name.js
@@ -0,0 +1,7 @@
+let pElements = document.getElementsByTagName('p'); //declares variable pElements, assigns value of all p elements
+
+console.log(pElements); // logs all p elements
+
+let secondH1 = document.getElementsByTagName('h1').item(1); //declares variable secondH1, assigns value of the second H1 element
+
+console.log(secondH1);
