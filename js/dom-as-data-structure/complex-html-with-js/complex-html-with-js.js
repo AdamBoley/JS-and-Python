8d91cfc1a85cc13d8d4daccdfc6868ef91f8c50e
AdamBoley <adamboley92@gmail.com>
2022-03-15 13:38:58 +0000

    notes

--- js/dom-as-data-structure/complex-html-with-js/complex-html-with-js.js
+++ js/dom-as-data-structure/complex-html-with-js/complex-html-with-js.js
@@ -38,24 +38,24 @@ function buildTable() {
             </tr>
         </thead>
         <tbody>
-    `;
+    `; //declares a variable to hold the head elements of a table - the top row and column names
     
-    for (book of books) {
+    for (book of books) { //book here is the variable representing the individual elements of the books array of objects. Each instance of book is an object
         
         let row = `
         <tr>
-            <td>${book.title}</td>
+            <td>${book.title}</td> 
             <td>${book.author}</td>
             <td>${book.published}</td>
         </tr>
-        `;
-        booksTableOpener += row;
+        `;//uses template literals to call each object's property and construct HTML to contain it
+        booksTableOpener += row; //appends each row to the table declared above
     }
     
     booksTableOpener += `
         </tbody>
     </table>
-    `;
+    `; //appends the closing tags to the table
 
     /*let booksTableCloser = `
     </tbody>
@@ -64,11 +64,12 @@ function buildTable() {
 
     /* 
     let booksTable = booksTableOpener + booksTableCloser
-    would also work, using a variable to join the two parts of the table*/
+    would also work, using a variable to join the two parts of the table
+    */
     
-    return booksTableOpener;
+    return booksTableOpener; //returns the booksTableOpener variable for later use outside the function
 }
 
-let table = buildTable();
+let table = buildTable(); // allows the returned booksTableOpener 
 
-document.getElementById('books-table').innerHTML = table;
+document.getElementById('books-table').innerHTML = table; //adds the table to the div with id of books-table
