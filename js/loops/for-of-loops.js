63fefef68d7ae0bd062ef77c491b398e9f61fe01
AdamBoley <adamboley92@gmail.com>
2022-03-16 13:07:17 +0000

    notes

--- js/loops/for-of-loops.js
+++ js/loops/for-of-loops.js
@@ -3,3 +3,7 @@ let food = ['pizza', 'steak', 'pasta', 'fruit salad', 'stir fry'];
 for (let i of food) { //i is the values of the array - pizza, steak, etc
   console.log(i); //logs i
 }
+
+//A For Of Loop is also known as a singular of plural loop, since the variable that holds the array is typically given a plural name - buttons, foods, cars, etc
+//The variable declared within the loop is typically given the singular name of the plural - button, food, car. This helps semantically tie them together
+//This is the slightly more modern way of defining loops, since there is no buggering about with variables called i and all that 
